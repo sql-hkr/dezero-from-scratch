3ea160eea437b8dea80d168b0f9ac698387e1639
sql-hkr <sql.hkr@gmail.com>
2025-09-28 23:48:20 +0900

    Create test_broadcast.py

---
+++ tests/test_broadcast.py
@@ -0,0 +1,15 @@
+import unittest
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+
+class TestBroadcast(unittest.TestCase):
+
+    def test_shape_check(self):
+        x = Variable(np.random.randn(1, 10))
+        b = Variable(np.random.randn(10))
+        y = x + b
+        loss = F.sum(y)
+        loss.backward()
+        self.assertEqual(b.grad.shape, b.shape)
